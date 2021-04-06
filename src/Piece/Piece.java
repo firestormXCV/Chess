2a77c724349e37683958d8b32d703d08be078ab9
TBrioche <72711057+TBrioche@users.noreply.github.com>
2021-04-06 20:49:57 +0200

    j

--- src/Piece/Piece.java
+++ src/Piece/Piece.java
@@ -3,5 +3,5 @@ package Piece;
 public interface Piece {
 	public void deplacer(int x, int y); 				//deplace une piece haut coordonnes indique, utilise possibledeplacement() dabord et si ca return false throw une exception
 	public boolean possibleDeplacement(int x, int y);   //verifie si le deplacement est possible en fonction des capacites de deplacement de la piece en suimpe et en attaque, de la limite du plateau et de l'occupation de la case par une piece allie
-	
+	public void attaquer(int x, int y);
 }
--- src/Piece/PieceAbs.java
+++ src/Piece/PieceAbs.java
@@ -9,18 +9,27 @@ public abstract class PieceAbs implements Piece {
 	private int[] attaque = new int [2];
 	
 	public PieceAbs(Couleur couleur, String nom, int positionInit[], int deplacement[], int attaque[]) {
-		assert(positionInit.length == 2 && deplacement.length == 2 && attaque.length == 2); //a virer ou faire un throw exception
+		assert(positionInit.length == 2 && deplacement.length == 2 && attaque.length == 2 && nom.length() == 1); //a virer ou faire un throw exception
 		
 		this.couleur = couleur;
-		this.nom = nom;
-		
+		this.nom = nom.toLowerCase();
+				
 		this.position[1] = positionInit[1];
 		this.position[2] = positionInit[2];
 		
 		this.attaque[1] = attaque[1];
-		this.attaque[2] = attaque[2];
+		this.attaque[2] = attaque[2];					//voir si il y a pas moyen de faire ca en 1 ligne au lieu de 2
 		
 		this.deplacement[1] = deplacement[1];
 		this.deplacement[2] = deplacement[2];
+	}
+	
+	
+	
+	public String toString() {
+		if (couleur == Couleur.blanc)
+			return this.nom.toUpperCase();
+		else 
+			return this.nom.toLowerCase();
 	}
 }
