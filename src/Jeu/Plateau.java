c5970e2a8c1f8c602f18282b2f6a7a02e0d39123
WhereIsYourLuck <48730787+WhereIsYourLuck@users.noreply.github.com>
2021-04-10 15:07:25 +0200

    Test toString

--- src/Jeu/Plateau.java
+++ src/Jeu/Plateau.java
@@ -1,5 +1,20 @@
 package Jeu;
 
 public class Plateau {
-
+	
+	public Plateau() {
+	}
+	
+	public String toString() {
+		StringBuilder Plateau = new StringBuilder();
+		Plateau.append("   a   b   c   d   e   f   g   h\n\n");
+		for(int i = 8 ; i > 0 ; i--) {
+			Plateau.append("  --- --- --- --- --- --- --- ---\n\n");
+			Plateau.append( i + " |   |   |   |   |   |   |   |   | " + i + "\n\\n");
+		}
+		Plateau.append("  --- --- --- --- --- --- --- ---\n\n");
+		Plateau.append("   a   b   c   d   e   f   g   h\n");
+		
+		return Plateau.toString();
+		}
 }
--- src/application/Appli.java
+++ src/application/Appli.java
@@ -1,10 +1,13 @@
 package application;
 
+import Jeu.Plateau;
+
 public class Appli {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Plateau test = new Plateau();
+		test.toString();
 	}
 
 }
