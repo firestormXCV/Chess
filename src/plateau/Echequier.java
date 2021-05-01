3a3e4581857ec500664386802ff010b0c5fda5d1
WhereIsYourLuck <48730787+WhereIsYourLuck@users.noreply.github.com>
2021-05-01 22:24:55 +0200

    aaa

--- src/plateau/Echequier.java
+++ src/plateau/Echequier.java
@@ -56,10 +56,28 @@ public class Echequier {
 	}
 	
 	public String toString() {
+		int x = 0;
+		int y = 0;
+		//x = piece.getPositionX();
+		//y = piece.getPositionY();
+		
+		String coordLettres = "    a   b   c   d   e   f   g   h";
+		
 		StringBuilder s = new StringBuilder();
+		s.append(coordLettres + "\n");
+		s.append("   --- --- --- --- --- --- --- ---\n");
+		for (int colonnes = 8 ; colonnes > 0 ; colonnes--) {
+			s.append(colonnes);
+			
+			for(int lignes = 9 ; lignes > 0 ; lignes--) {
+				s.append(" | " + " ");
+			}
+			
+			s.append(colonnes + "\n");
+			s.append("   --- --- --- --- --- --- --- ---\n");
+		}
 		
-		
-		return s.toString();
-		
+		s.append(coordLettres + "\n");
+		return s.toString();	
 	}
 }
